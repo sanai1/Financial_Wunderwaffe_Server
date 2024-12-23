104dc5c288a465a4d384e7e49fa8e593a059986d
aleksandr <sanek.v.07@gmail.com>
2024-12-23 23:53:12 +0300

    Initial commit

---
+++ src/main/kotlin/org/example/financial_wunderwaffe_server/FinancialWunderwaffeServerApplication.kt
@@ -0,0 +1,11 @@
+package org.example.financial_wunderwaffe_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialWunderwaffeServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinancialWunderwaffeServerApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/financial_wunderwaffe_server/FinancialWunderwaffeServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.financial_wunderwaffe_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancialWunderwaffeServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
